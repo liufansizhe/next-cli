da36b1851204572108792423a7a62b584b6c70b4
lfsz <651828515@qq.com>
2024-12-06 16:56:49 +0800

    同步代码

--- src/app/api/a/route.ts
+++ src/app/api/a/route.ts
@@ -3,7 +3,7 @@ import { ResponseParams, ResponseType } from "@/app/lib/types";
 import db from "@/app/lib/db/index";
 import { requestHandle } from "@/app/utils/requestHandle";
 
-export async function POST(request: object, params: ResponseParams) {
+export async function GET(request: object, params: ResponseParams) {
   const data = await (await db.find("users")).toArray();
   console.log("lfsz--", process.env.NEXT_PUBLIC_API);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  return <h1>Hello, Next.js!</h1>;
 }
